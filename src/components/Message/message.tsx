d2284432754f9cae2acf81ee12d926dc3128e390
yazhao.hou <yazhao.hou@upai.com>
2022-06-20 15:07:24 +0800

    feat: message

---
+++ src/components/Message/message.tsx
@@ -0,0 +1,44 @@
+import React, { FC, ReactElement } from 'react'
+export type MessageType = 'info' | 'success' | 'danger' | 'warning'
+
+export interface MessageProps {
+  text: string
+  type: MessageType
+}
+
+const Message: FC<MessageProps> = (props: MessageProps) => {
+  const { text, type } = props
+
+  // const renderIcon = (messageType: MessageType): ReactElement => {
+  //   let messageIcon: IconProp
+
+  //   switch (messageType) {
+  //     case 'success':
+  //       messageIcon = 'check-circle'
+  //       break
+  //     case 'danger':
+  //       messageIcon = 'times-circle'
+  //       break
+  //     case 'warning':
+  //       messageIcon = 'exclamation-circle'
+  //       break
+  //     case 'info':
+  //     default:
+  //       messageIcon = 'info-circle'
+  //       break
+  //   }
+
+  //   return <Icon icon={messageIcon} theme={messageType} />
+  // }
+
+  return (
+    <div className='message'>
+      <div className='message-content'>
+        {/* <div className='icon'>{renderIcon(type)}</div> */}
+        <div className='text'>{text}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Message
